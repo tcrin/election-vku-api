c092dc64d9e01e485d612a6a12d1427208606952
Tran Cong Rin <trancongrin2k.it@gmail.com>
2023-02-19 11:28:19 +0700

    update connect 1

--- app/common/connect.js
+++ app/common/connect.js
@@ -1,42 +1,66 @@
 const { Client, Pool } = require("pg");
 
-// const client = new Client({
-//   connectionString: "postgres://tcrin:WgODMoykqLKgQqCFzhXBZFRtakMP925y@dpg-cfno5cirrk0eqlq8j8i0-a.singapore-postgres.render.com/electionvkudb" ,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
-
 const client = new Client({
-  user: 'tcrin',
-  host: 'dpg-cfno5cirrk0eqlq8j8i0-a.singapore-postgres.render.com',
-  database: 'electionvkudb',
-  password: 'WgODMoykqLKgQqCFzhXBZFRtakMP925y',
-  port: 5432,
+  connectionString: "postgres://tcrin:WgODMoykqLKgQqCFzhXBZFRtakMP925y@dpg-cfno5cirrk0eqlq8j8i0-a.singapore-postgres.render.com/electionvkudb" ,
   ssl: {
     rejectUnauthorized: false,
   },
-  idleTimeoutMillis: 0,
-  connectionTimeoutMillis: 0,
-})
+});
 
-client.connect((err) => {
-  if (err) {
-    console.error('Error during connection:', err);
-    return;
+async function connectToDatabase() {
+  try {
+    await client.connect((err) => {
+      if (err) {
+        console.error('Error during connection:', err);
+        return;
+      }
+      console.log('Connected to database');
+    });
+    
+    // Xử lý sự kiện lỗi kết nối
+    client.on('error', (err) => {
+      console.error('Database error:', err);
+    });
+    
+    // Xử lý sự kiện ECONNRESET
+    client.on('end', () => {
+      console.log('Database connection terminated');
+    });
+  } catch (err) {
+    console.error('Error connecting to the database', err);
+    // Reconnect to the database
+    await client.connect((err) => {
+      if (err) {
+        console.error('Error during connection:', err);
+        return;
+      }
+      console.log('Reconnected to database');
+    });
+    
+    // Xử lý sự kiện lỗi kết nối
+    client.on('error', (err) => {
+      console.error('Database error:', err);
+    });
+    
+    // Xử lý sự kiện ECONNRESET
+    client.on('end', () => {
+      console.log('Database connection terminated');
+    });
   }
-  console.log('Connected to database');
-});
+}
 
-// Xử lý sự kiện lỗi kết nối
-client.on('error', (err) => {
-  console.error('Database error:', err);
-});
+// const client = new Client({
+//   user: 'tcrin',
+//   host: 'dpg-cfno5cirrk0eqlq8j8i0-a.singapore-postgres.render.com',
+//   database: 'electionvkudb',
+//   password: 'WgODMoykqLKgQqCFzhXBZFRtakMP925y',
+//   port: 5432,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// })
 
-// Xử lý sự kiện ECONNRESET
-client.on('end', () => {
-  console.log('Database connection terminated');
-});
+connectToDatabase()
 
 module.exports = client;
 //"postgres://avqqhagrdcqtth:9d597bee2ef6341a87fa70ce04f90f9c05ca27d9cdd39e3dbb937b16395fa9d1@ec2-3-229-252-6.compute-1.amazonaws.com:5432/dde778vl6hjco9"
